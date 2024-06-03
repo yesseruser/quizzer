00848c738b4366f5a7c27d164db3c07187b9d003
yesseruser <67008763+yesseruser@users.noreply.github.com>
2024-06-03 15:40:38 +0000

    Capitalize Q in README, add supabase client, fix redirect

--- js/home.js
+++ js/home.js
@@ -1,3 +1,8 @@
+const db = supabase.createClient(
+    "https://mtjuyzexwsfhhidifrze.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im10anV5emV4d3NmaGhpZGlmcnplIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc0Mjc3ODksImV4cCI6MjAzMzAwMzc4OX0.my7S9Xq0gTUxhiq0F7VOsnDFR-2B4uoG9r1qBrf8EzU"
+)
+
 PetiteVue.createApp({
     questions: [{text: "", options: ["", "", "", ""], correct: 0}],
     addQuestion() {
