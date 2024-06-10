1a9e4c27360bcd585f4f6c0ca429c905b2262a55
yesseruser <67008763+yesseruser@users.noreply.github.com>
2024-06-10 15:28:14 +0000

    Add question loading

--- js/quiz.js
+++ js/quiz.js
@@ -2,9 +2,11 @@ const db = supabase.createClient(
     "https://mtjuyzexwsfhhidifrze.supabase.co",
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im10anV5emV4d3NmaGhpZGlmcnplIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc0Mjc3ODksImV4cCI6MjAzMzAwMzc4OX0.my7S9Xq0gTUxhiq0F7VOsnDFR-2B4uoG9r1qBrf8EzU"
 )
+const params = new URL(location.href).searchParams;
 
-let questions = [{text: "Oholera", options: ["Čitu fredy fasbér?", "Chitu freddy Fazbear", " i aint reading allthat", "TL;DR I am not reading all that."], correct: 0}];
-
-PetiteVue.createApp({
-    questions: questions,
-})
+db.from("quizzes").select("*").eq("id", params.get("id")).then((response) => {
+    PetiteVue.createApp({
+        preview: params.get("preview") === "true",
+        questions: response.data[0].data,
+    }).mount();
+});
